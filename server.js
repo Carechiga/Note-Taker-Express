cb49ffe2b7200ef00833cf8073bbafee9c1d1a61
Carechiga <Carechiga43@gmail.com>
2023-03-14 08:11:13 -0700

    functionality mostly complete

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const path = require('path');
 const api = require('./routes/index.js');
-
+const path = require('path');
 const PORT = process.env.port || 3001;
 
 const app = express();
@@ -16,8 +15,8 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
